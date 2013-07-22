f3e55bba5a91f0ccdd764c96c309b1bfb2c5907f
Namukaba Hichilo <hichilon@berea.edu>
2013-07-22 15:50:13 -0400

    DataPacket Library v1.0

---
+++ DataPacket.cpp
@@ -0,0 +1,59 @@
+#include "Arduino.h"
+#include "DataPacket.h"
+
+DataPacket::DataPacket(){
+	_temp = 0.0;
+	_humidity = 0.0;
+	_lowpulseoccupancy = 0;
+	_ratio = 0.0;
+	_concentration = 0.0;
+	_airquality = 0;
+}
+
+void DataPacket::print_packet(){
+	Serial.println(_temp);
+	Serial.println(_humidity);
+	Serial.println(_lowpulseoccupancy);
+	Serial.println(_ratio);
+	Serial.println(_concentration);
+	Serial.println(_airquality);
+}
+
+int DataPacket::get_airquality(){
+	return _airquality;
+}
+float DataPacket::get_temp(){
+	return _temp;
+}
+float DataPacket::get_humid(){
+	return _humidity;
+}
+unsigned long DataPacket::get_lowpulseoccupancy(){
+	return _lowpulseoccupancy;
+}
+float DataPacket::get_ratio(){
+	return _ratio;
+}
+float DataPacket::get_concentration(){
+	return _concentration;
+}
+
+
+void DataPacket::set_airquality(int airquality){
+	_airquality = airquality;
+}		
+void DataPacket::set_temp(float temp){
+	_temp = temp;
+}
+void DataPacket::set_humid(float humid){
+	_humidity = humid;
+}
+void DataPacket::set_lowpulseoccupancy(unsigned long lowpulseoccupancy){
+	_lowpulseoccupancy = lowpulseoccupancy;
+}
+void DataPacket::set_ratio(float ratio){
+	_ratio = ratio;
+}
+void DataPacket::set_concentration(float concentration){
+	_concentration = concentration;
+}
---
+++ DataPacket.h
@@ -0,0 +1,31 @@
+#ifndef DataPacket_h
+#define DataPacket_h
+
+#include "Arduino.h"
+
+class DataPacket{
+	public:
+		DataPacket();
+		void print_packet();
+		float get_temp();
+		float get_humid();
+		unsigned long get_lowpulseoccupancy();
+		float get_ratio();
+		float get_concentration();
+		int get_airquality();
+		
+		void set_temp(float temp);
+		void set_humid(float humid);
+		void set_lowpulseoccupancy(unsigned long lowpulseoccupancy);
+		void set_ratio(float ratio);
+		void set_concentration(float concentration);
+		void set_airquality(int airquality);
+	private:
+		float _temp;
+		float _humidity;
+		unsigned long _lowpulseoccupancy;
+		float _ratio;
+		float _concentration;
+		int _airquality;
+};
+#endif
